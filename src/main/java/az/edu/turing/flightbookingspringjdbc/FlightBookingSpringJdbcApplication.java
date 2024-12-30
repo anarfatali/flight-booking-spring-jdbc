e01beeefeef04791fc16cd319b5dc13e05e283d8
anarouis <anarftliyev@gmail.com>
2024-12-30 23:38:03 +0400

    Initial commit

---
+++ src/main/java/az/edu/turing/flightbookingspringjdbc/FlightBookingSpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.flightbookingspringjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingSpringJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightBookingSpringJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/edu/turing/flightbookingspringjdbc/FlightBookingSpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.flightbookingspringjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSpringJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
